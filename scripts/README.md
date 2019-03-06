577e96aa93c4ad8f57c2c37668d21724d608e6c7
neocliff <neocliff@mac.com>
2019-03-06 15:29:06 -0600

    script updates

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,9 @@
 # Scripts Repository
 
 Useful scripts to automate boring things!
+
+- init-for-re.sh - this script initializes an Ubuntu-ish system
+	a bunch of packages that are use for re activities.
+	things like compilers, assemblers, etc.
+- make-key-pair.sh - quick script to use ssh-keygen to make
+	a new private keypair and then add it to ssh-agent.
--- scripts/init-for-re.sh
+++ scripts/init-for-re.sh
@@ -3,22 +3,9 @@
 # this script automates a bunch of installation items for creating the
 # vm's use for reverse engineering binaries.
 
-sudo apt-get install -y gcc g++
+sudo apt-get install -y gcc r++
 sudo apt-get install -y cmake
 sudo apt-get install -y tmux
 sudo apt-get install -y ssh
 sudo apt-get install -y git
 sudo apt-get install -y wine-stable
-
-# generate the key for git
-cd ~
-echo "running ssh-key to create a new id_rsa and id_rsa.pub..."
-ssh-keygen -t rsa -b 4096 -C "neocliff@mac.com"
-
-echo "starting ssh-agent..."
-eval "$(ssh-agent -s)"
-
-echo "adding id_rsa to ssh-agent..."
-ssh-add ~/.ssh/id_rsa
-
-
