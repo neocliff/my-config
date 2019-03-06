47895daeed68f619f0cafc936c7f8fa2db774cc1
neocliff <neocliff@mac.com>
2019-03-06 15:31:38 -0600

    add nasm to init-for-re.sh

--- scripts/init-for-re.sh
+++ scripts/init-for-re.sh
@@ -4,6 +4,7 @@
 # vm's use for reverse engineering binaries.
 
 sudo apt-get install -y gcc r++
+sudo apt-get -y nasm
 sudo apt-get install -y cmake
 sudo apt-get install -y tmux
 sudo apt-get install -y ssh
