3cc9fc054e0b9f8043eebb9fc0554650ed56f7aa
Cliff Williams <cwilliams@pop-os.localdomain>
2019-03-26 08:26:21 -0500

    update init-for-re2.sh with additional packages

--- scripts/init-for-re2.sh
+++ scripts/init-for-re2.sh
@@ -3,30 +3,41 @@
 # this script automates a bunch of installation items for creating
 # a system to use for reverse engineering binaries.
 
-# install programming languages support
-sudo apt-get install -y gcc g++
-sudo apt-get -y nasm
-sudo apt-get install -y cmake
-sudo apt-get install -y python3-pip
-sudo apt-get install -y pylint
-sudo apt-get install -y build-essential
+# install compilers and interpreters and such
+#     note that build-essential gives you gcc, g++, as, etc
+sudo apt install -y build-essential
+sudo apt install -y nasm
+sudo apt install -y cmake
+sudo apt install -y python-pip
+sudo apt install -y python3-pip
+sudo apt install -y pylint
 
 # we are always going to be making virtual machines for things
-sudo apt-get install -y virtualbox-qt
-sudo apt-get install -y qemu
+sudo apt install -y virtualbox-qt
+sudo apt install -y qemu
+
+# we might want to use containers as well.
+sudo apt install docker.io
 
 # tmux is a terminal multiplexer; much nicer than 47 terminal windows
-sudo apt-get install -y tmux
+sudo apt install -y tmux
+
+# install various editors
+sudo apt install -y vim
+sudo apt install -y bless
 
 # wine provides the windows system api's that translate windows calls
-# to linux calls
-sudo apt-get install -y wine-stable
+# to linux calls. you only need this if you want to RE windows binaries
+# on a linux platform.
+sudo apt install -y wine-stable
 
 # install a fuzzer
-sudo apt-get install -y zzuf
+sudo apt install -y zzuf
 
-# install OpenJDK 11 JDK
-sudo apt-get install openjdk-11-jdk
+# install OpenJDK 11 JDK. note that this only gets you JDK 11 on an
+# ubuntu 18.10 system. if you do this on an ubuntu 18.04 system you
+# get JDK 10 instead. you have been warned!
+sudo apt install -y openjdk-11-jdk
 
 # do a final update/upgrade
 sudo apt update
